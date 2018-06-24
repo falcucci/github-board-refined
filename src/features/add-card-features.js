4fdb7f977138a7fefc875d2bbbe0cca896ce983d
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2018-06-23 21:28:41 -0300

    remove log

--- src/features/add-card-features.js
+++ src/features/add-card-features.js
@@ -1,13 +1,19 @@
 import { observe } from 'selector-observer'
 import { addHistoryProgressBar }from './add-history-progress-bar'
+import { hideReferences } from './hide-references'
 
 export const addCardFeatures = async () => {
 
   observe("div[id^='card-']", {
     add(el) {
-      addHistoryProgressBar(el)
+      cardFeatures(el)
       // el.addEventListener('onshow', addProgressBar)
     },
   })
 
+}
+
+export const cardFeatures = async el => {
+  addHistoryProgressBar(el)
+  hideReferences(el)
 }
