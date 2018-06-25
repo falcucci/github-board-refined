239845b122befdb8620e961785a84b75690ef630
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2018-06-25 04:02:06 -0300

    trying to get preview token

--- src/content.js
+++ src/content.js
@@ -1,7 +1,7 @@
 import 'webext-dynamic-content-scripts'
 import {
   safeElementReady,
-  shouldProceed
+  shouldProceed,
 } from './utils'
 import {
   applyFullScreen,
@@ -11,12 +11,21 @@ import {
   addMarkdownPreview,
 } from './features'
 
-async function init() {
+const init = async () => {
+  import select from 'select-dom'
   await safeElementReady('body')
   if (!shouldProceed()) {
     return
   }
+  microAjax("https://github.com/falcucci/github-board-refined/issues/2/show_from_project", function (res) {
+    var div = document.createElement('div');
+    div.innerHTML = res.trim();
+    console.log('div.firstChild: ', div.firstChild);
 
+    // let a = select('.js-suggester-container', div)
+    // console.log('a: ', a);
+
+  })
   addCardFeatures()
   addRelatedPullRequest()
   addNoteTemplate()
