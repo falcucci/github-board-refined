faf9a2c8d6cae8a0670f1bff6ae6161fd0c119e8
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2018-06-25 14:18:03 -0300

    minor changes

--- src/options.js
+++ src/options.js
@@ -5,7 +5,7 @@ new OptionsSync().syncForm('#options-form');
 chrome.runtime.getPlatformInfo(info => {
   if (info.os === 'linux') {
     setTimeout(() => {
-      document.body.style['min-width']= '600px';
+      document.body.style['min-width'] = '600px';
     }, 50);
   }
 
