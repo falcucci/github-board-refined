b70ed263bfbcefc2b99687baec910866be22bd19
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2018-06-25 11:50:07 -0300

    fix chrome layout width bug

--- src/options.js
+++ src/options.js
@@ -1,3 +1,12 @@
 import OptionsSync from 'webext-options-sync'
 
 new OptionsSync().syncForm('#options-form');
+
+chrome.runtime.getPlatformInfo(info => {
+  if (info.os === 'linux') {
+    setTimeout(() => {
+      document.body.style['min-width']= '600px';
+    }, 50);
+  }
+
+});
