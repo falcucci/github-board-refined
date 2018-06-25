c8b56075e40fe9ba568ff1986e5802c267d33163
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2018-06-25 14:38:52 -0300

    fix linux/chrome layout width

--- src/options.js
+++ src/options.js
@@ -1,9 +1,12 @@
 import OptionsSync from 'webext-options-sync'
+import _ from 'lodash'
 
 new OptionsSync().syncForm('#options-form');
 
+const chromeVersion = /Chrome\/([0-9.]+)/.exec(navigator.userAgent)[1];
+
 chrome.runtime.getPlatformInfo(info => {
-  if (info.os === 'linux') {
+  if (info.os === 'linux' && !_.isEmpty(chromeVersion)) {
     setTimeout(() => {
       document.body.style['min-width'] = '600px';
     }, 50);
