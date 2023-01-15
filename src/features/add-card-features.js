6cc8adf40f6a8097dad9e8ef58bb00f4a6e5738b
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-01-15 21:35:22 +0100

    refactor card features
    refactors the code for the `addcardfeatures` function and the `cardfeatures` function in `src/features/add-card-features.js`. the changes include:
    
    - replacing single quotes with double quotes for string literals
    - adding semicolons to the end of each statement
    - removing unnecessary blank lines
    - adding a semicolon to the end of the `cardfeatures` function call in the `add` method
    
    ```diff
    diff --git a/src/features/add-card-features.js b/src/features/add-card-features.js
    index 7f363d9e20b0..e220cc1a8ace 100644
    --- a/src/features/add-card-features.js
    +++ b/src/features/add-card-features.js
    @@ -1,21 +1,19 @@
    -import { observe } from 'selector-observer'
    -import { addhistoryprogressbar }from './add-history-progress-bar'
    -import { hidereferences } from './hide-references'
    -import { addlabels } from './add-labels'
    +import { observe } from \"selector-observer\";
    +import { addhistoryprogressbar } from \"./add-history-progress-bar\";
    +import { hidereferences } from \"./hide-references\";
    +import { addlabels } from \"./add-labels\";
    
     export const addcardfeatures = async () => {
    -
       observe(\"div[id^='card-']\", {
         add(el) {
    -      cardfeatures(el)
    +      cardfeatures(el);
           // el.addeventlistener('onshow', addprogressbar)
         },
    -  })
    -
    -}
    +  });
    +};
    
     export const cardfeatures = async el => {
    -  addlabels(el)
    -  addhistoryprogressbar(el)
    -  hidereferences(el)
    -}
    +  addlabels(el);
    +  addhistoryprogressbar(el);
    +  hidereferences(el);
    +};
    ```

--- src/features/add-card-features.js
+++ src/features/add-card-features.js
@@ -1,21 +1,19 @@
-import { observe } from 'selector-observer'
-import { addHistoryProgressBar }from './add-history-progress-bar'
-import { hideReferences } from './hide-references'
-import { addLabels } from './add-labels'
+import { observe } from "selector-observer";
+import { addHistoryProgressBar } from "./add-history-progress-bar";
+import { hideReferences } from "./hide-references";
+import { addLabels } from "./add-labels";
 
 export const addCardFeatures = async () => {
-
   observe("div[id^='card-']", {
     add(el) {
-      cardFeatures(el)
+      cardFeatures(el);
       // el.addEventListener('onshow', addProgressBar)
     },
-  })
-
-}
+  });
+};
 
 export const cardFeatures = async el => {
-  addLabels(el)
-  addHistoryProgressBar(el)
-  hideReferences(el)
-}
+  addLabels(el);
+  addHistoryProgressBar(el);
+  hideReferences(el);
+};
